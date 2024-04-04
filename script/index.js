4b87b6c145834978ac86f0da7dbfc09a400edc63
Екатерина Козлова <kate.rina.kozlova@yandex.ru>
2024-04-04 20:48:42 +0600

    подключить скрипт

---
+++ script/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const formAddProduct = document.querySelector('.modal__form');
+const discountCheckbox = document.querySelector('#discount');
+const discountInput = document.querySelector('.modal__input_discount');
