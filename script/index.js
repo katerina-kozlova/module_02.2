65bb2b3aab0336c2c44b7c4fb4d6761e515dcff1
Екатерина Козлова <kate.rina.kozlova@yandex.ru>
2024-04-05 17:48:34 +0600

    создать таблицу на основе массива

--- script/index.js
+++ script/index.js
@@ -4,3 +4,88 @@ const modalTitle = document.querySelector('.modal__title');
 const formAddProduct = document.querySelector('.modal__form');
 const discountCheckbox = document.querySelector('#discount');
 const discountInput = document.querySelector('.modal__input_discount');
+
+const overlay = document.querySelector('.overlay');
+overlay.classList.remove('active');
+
+const createRow = (obj) => {
+    const createLine = document.createElement('tr');
+
+    for (const key in obj) {
+        const createCell = document.createElement('td');
+        createCell.textContent = obj[key];
+        createLine.appendChild(createCell);
+    }
+    return createLine;
+};
+
+const renderGoods = (arr) => {
+    const table = document.createElement('table');
+
+    arr.forEach((obj) => {
+        const row = createRow(obj); 
+        table.appendChild(row);
+    });
+    document.body.appendChild(table);
+};
+
+const goods = [
+    {
+      "id": 1,
+      "title": "Смартфон Xiaomi 11T 8/128GB",
+      "price": 27000,
+      "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
+      "category": "mobile-phone",
+      "discont": false,
+      "count": 3,
+      "units": "шт",
+      "images": {
+        "small": "img/smrtxiaomi11t-m.jpg",
+        "big": "img/smrtxiaomi11t-b.jpg"
+      }
+    },
+    {
+      "id": 2,
+      "title": "Радиоуправляемый автомобиль Cheetan",
+      "price": 4000,
+      "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
+      "category": "toys",
+      "discont": 5,
+      "count": 1,
+      "units": "шт",
+      "images": {
+        "small": "img/cheetancar-m.jpg",
+        "big": "img/cheetancar-b.jpg"
+      }
+    },
+    {
+      "id": 3,
+      "title": "ТВ приставка MECOOL KI",
+      "price": 12400,
+      "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
+      "category": "tv-box",
+      "discont": 15,
+      "count": 4,
+      "units": "шт",
+      "images": {
+        "small": "img/tvboxmecool-m.jpg",
+        "big": "img/tvboxmecool-b.jpg"
+      }
+    },
+    {
+      "id": 4,
+      "title": "Витая пара PROConnect 01-0043-3-25",
+      "price": 22,
+      "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
+      "category": "cables",
+      "discont": false,
+      "count": 420,
+      "units": "v",
+      "images": {
+        "small": "img/lan_proconnect43-3-25.jpg",
+        "big": "img/lan_proconnect43-3-25-b.jpg"
+      }
+    }
+  ];
+
+  renderGoods(goods);
