d388d02cce24be65820fa2fa039e74dd0fcc2ad8
Екатерина Козлова <kate.rina.kozlova@yandex.ru>
2024-04-06 15:21:43 +0600

    добавить массив в созданную в верстке таблицу

--- script/index.js
+++ script/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import goods from './goods.js';
+
 const modalTitle = document.querySelector('.modal__title');
 const formAddProduct = document.querySelector('.modal__form');
 const discountCheckbox = document.querySelector('#discount');
@@ -9,83 +11,76 @@ const overlay = document.querySelector('.overlay');
 overlay.classList.remove('active');
 
 const createRow = (obj) => {
-    const createLine = document.createElement('tr');
+    const tableRow = document.createElement('tr');
+    const cellFirst = document.createElement('td');
+    const cellSecond = document.createElement('td');
+    const cellSecondSpan = document.createElement('span');
+    const cellThird = document.createElement('td');
+    const cellFourth = document.createElement('td');
+    const cellFIfth = document.createElement('td');
+    const cellSixth = document.createElement('td');
+    const cellSeventh = document.createElement('td');
+    const cellEighth = document.createElement('td');
+    const btnPic = document.createElement('button');
+    const btnEdit = document.createElement('button');
+    const btnDel = document.createElement('button');
 
-    for (const key in obj) {
-        const createCell = document.createElement('td');
-        createCell.textContent = obj[key];
-        createLine.appendChild(createCell);
-    }
-    return createLine;
+    cellFirst.textContent = obj.id;
+    cellFirst.classList.add('table__cell');
+    tableRow.append(cellFirst);
+
+    cellSecond.classList.add('table__cell', 'table__cell_left', 'table__cell_name');
+    cellSecond.setAttribute('data-id', obj.id);
+
+    cellSecondSpan.classList.add('table__cell-id');
+    cellSecondSpan.textContent = 'id: ' + obj.id;
+    cellSecond.append(cellSecondSpan);
+    cellSecond.append(document.createTextNode(obj.title));
+    tableRow.append(cellSecond);
+
+    cellThird.classList.add('table__cell', 'table__cell_left');
+    cellThird.textContent = obj.category;
+    tableRow.append(cellThird);
+
+    cellFourth.classList.add('table__cell');
+    cellFourth.textContent = obj.units;
+    tableRow.append(cellFourth);
+
+    cellFIfth.classList.add('table__cell');
+    cellFIfth.textContent = obj.count;
+    tableRow.append(cellFIfth);
+
+    cellSixth.classList.add('table__cell');
+    cellSixth.textContent = '$' + obj.price;
+    tableRow.append(cellSixth);
+
+    cellSeventh.classList.add('table__cell');
+    cellSeventh.textContent = `$${obj.price * obj.count}`;
+    tableRow.append(cellSeventh);
+
+    cellEighth.classList.add('table__cell', 'table__cell_btn-wrapper');
+
+    btnPic.classList.add('table__btn', 'table__btn_pic');
+    cellEighth.append(btnPic);
+
+    btnEdit.classList.add('table__btn', 'table__btn_edit');
+    cellEighth.append(btnEdit);
+
+    btnDel.classList.add('table__btn', 'table__btn_del');
+    cellEighth.append(btnDel);
+
+    tableRow.append(cellEighth);
+
+    return tableRow;
 };
 
 const renderGoods = (arr) => {
-    const table = document.createElement('table');
+    const tableBody = document.querySelector('.table__body');
 
-    arr.forEach((obj) => {
+    arr.map((obj) => {
         const row = createRow(obj); 
-        table.appendChild(row);
+        tableBody.append(row);
     });
-    document.body.appendChild(table);
 };
 
-const goods = [
-    {
-      "id": 1,
-      "title": "Смартфон Xiaomi 11T 8/128GB",
-      "price": 27000,
-      "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
-      "category": "mobile-phone",
-      "discont": false,
-      "count": 3,
-      "units": "шт",
-      "images": {
-        "small": "img/smrtxiaomi11t-m.jpg",
-        "big": "img/smrtxiaomi11t-b.jpg"
-      }
-    },
-    {
-      "id": 2,
-      "title": "Радиоуправляемый автомобиль Cheetan",
-      "price": 4000,
-      "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
-      "category": "toys",
-      "discont": 5,
-      "count": 1,
-      "units": "шт",
-      "images": {
-        "small": "img/cheetancar-m.jpg",
-        "big": "img/cheetancar-b.jpg"
-      }
-    },
-    {
-      "id": 3,
-      "title": "ТВ приставка MECOOL KI",
-      "price": 12400,
-      "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
-      "category": "tv-box",
-      "discont": 15,
-      "count": 4,
-      "units": "шт",
-      "images": {
-        "small": "img/tvboxmecool-m.jpg",
-        "big": "img/tvboxmecool-b.jpg"
-      }
-    },
-    {
-      "id": 4,
-      "title": "Витая пара PROConnect 01-0043-3-25",
-      "price": 22,
-      "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
-      "category": "cables",
-      "discont": false,
-      "count": 420,
-      "units": "v",
-      "images": {
-        "small": "img/lan_proconnect43-3-25.jpg",
-        "big": "img/lan_proconnect43-3-25-b.jpg"
-      }
-    }
-  ];
-
-  renderGoods(goods);
+renderGoods(goods);
